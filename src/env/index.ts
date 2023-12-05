60184a50d9cdaab94d812954d2a965ac7c1fcdd3
Gabriel <gabrielgrazeffi123@gmail.com>
2023-12-05 17:52:16 -0300

    add plugins

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,16 +3,16 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_URL: z.string(),
+  DATABASE_URL: z.string().default('db/app.db'),
   PORT: z.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
-  console.error('Invalid enviroment variables!', _env.error.format())
+if (_env.success === false) {
+  console.error('⚠️ Invalid environment variables', _env.error.format())
 
-  throw new Error('Invalid enviroment variables!')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
